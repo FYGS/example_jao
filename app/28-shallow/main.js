13562b483a2a7aada73a6282d5e6900ec6def0f6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-13 18:01:45 +0200

    spread

--- app/28-shallow/main.js
+++ app/28-shallow/main.js
@@ -13,3 +13,6 @@ const q = { ...o };
 console.log('p', p);
 console.log('p === o', p === o);
 console.log('p.b === o.b', p.b === o.b);
+console.log('q', q);
+console.log('q === o', q === o);
+console.log('q.b === o.b', q.b === o.b);
