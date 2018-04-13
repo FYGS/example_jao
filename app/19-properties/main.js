fa051bd26845467b9df06ad182edb08d01d2bfd7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-13 10:45:24 +0200

    property

--- app/19-properties/main.js
+++ app/19-properties/main.js
@@ -1,22 +1,25 @@
+'use strict';
+
 const james = {
     name: 'Bond',
     age: 47
 };
 james.size = 1.85;
-james[007] = 'licence to kill';
+// not allowed in strict mode
+// james[007] = 'licence to kill';
+james[7] = 'where is my 00?'
 james.$$$ = 'casino';
 james.___ = 'invisible';
 james['#$%^&'] = 'don\'t be so rude please!';
 Object.defineProperty(james, 'code', {
     value: '007',
     writable: false,
-    configurable: false,
-    enumerable: true,
 });
 
 console.log('james.code', james.code);
 
 try {
+    // will throw an error only in strict mode.
     james.code = '009';
 } catch (e) {
     console.log('error', e.message);
