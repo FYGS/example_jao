7a4cde15b8f2a10ecbcc10ca079790c5ce1f4350
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-13 17:06:29 +0200

    clone

--- app/27-clone/main.js
+++ app/27-clone/main.js
@@ -1,6 +1,12 @@
-const a = 3;
-const b = a;
+let a = 3;
+let b = a;
 
 a = 5;
+console.log('a', a);
+console.log('b', b);
+
+a = {x: 2};
+b = a;
+a.x = 7;
 console.log('a', a);
 console.log('b', b);
