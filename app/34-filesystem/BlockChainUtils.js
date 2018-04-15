71f5f4e78dd6f2fe034d677b7058ad518fa820ed
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-15 13:24:47 +0200

    console

--- app/34-filesystem/BlockChainUtils.js
+++ app/34-filesystem/BlockChainUtils.js
@@ -14,5 +14,6 @@ module.exports.BlockChainUtils = class BlockChainUtils {
             `${block.id};${block.data};${block.hash}
 `, '');
         fs.writeFileSync(filename, accumulator);
+        console.log('file written.');
     }
 }
