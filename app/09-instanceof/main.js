83722de93099fb372c42fd3a00e4b0870cdf63f8
Maite THOMIAS <maite.thomias@gmail.com>
2018-04-16 09:48:50 +0200

    ok

--- app/09-instanceof/main.js
+++ app/09-instanceof/main.js
@@ -1,5 +1,6 @@
 console.log('3 instanceof Object', 3 instanceof Object);
-console.log('"hello" instanceof Object', 'hello' instanceof Object);
+console.log('"hello" instanceof Object', 'hello' instanceof String);
+console.log('new String("hello") instanceof Object', new String('hello') instanceof String);
 console.log('undefined instanceof Object', undefined instanceof Object);
 console.log('null instanceof Object', null instanceof Object);
 
@@ -11,4 +12,3 @@ console.log('class {} instanceof Object', class {} instanceof Object);
 // a little bit deeper...
 console.log('Object instanceof Function', Object instanceof Function);
 console.log('Function instanceof Object', Function instanceof Object);
-
