77baec7cac4b0dc39b9cb6697641bf6ef9a39883
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-02 09:06:11 +0100

    updated versions

--- app/38-typescript/src/grid-example.ts
+++ app/38-typescript/src/grid-example.ts
@@ -2,63 +2,151 @@ import { Grid } from "./grid";
 import { Point } from "./Point";
 
 export class GridExample {
+  points: Set<Point>;
 
-    points: Set<Point>;
+  constructor(public grid: Grid) {}
 
-    constructor(public grid: Grid) { }
+  getMakeMethodNames() {
+    return Object.getOwnPropertyNames(GridExample.prototype).filter((m) =>
+      m.startsWith("make")
+    );
+  }
 
-    getMakeMethodNames() {
-        return Object.getOwnPropertyNames(GridExample.prototype).filter(m => m.startsWith('make'));
+  set(str: string): void {
+    console.log("str", str);
+    this.grid.hardReset();
+    const methodNames = this.getMakeMethodNames().filter(
+      (m) => m.substr("make".length) === str
+    );
+
+    if (methodNames.length === 0) {
+      throw new Error("Make method not found.");
     }
 
-    set(str: string): void {
-        console.log('str', str);
-        this.grid.hardReset();
-        const methodNames = this.getMakeMethodNames().filter(m => m.substr('make'.length) === str);
+    this[methodNames[0]]();
+    this.grid.add(this.points);
+  }
 
-        if (methodNames.length === 0) {
-            throw new Error('Make method not found.');
-        }
+  buildSet(array: number[][]) {
+    const c: Point = this.grid.getCenter();
+    this.points = array.reduce((acc, p) => {
+      acc.add(new Point(c.x - p[1], c.y + p[0]));
+      return acc;
+    }, new Set<Point>());
+  }
 
-        this[methodNames[0]]();
-        this.grid.add(this.points);
-    }
+  makeEmpty() {
+    this.buildSet([]);
+  }
 
-    buildSet(array: number[][]) {
-        const c: Point = this.grid.getCenter();
-        this.points = array.reduce((acc, p) => {
-            acc.add(new Point(c.x - p[1], c.y + p[0]));
-            return acc;
-        }, new Set());
-    }
+  makePentominoR() {
+    this.buildSet([
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, -1],
+    ]);
+  }
 
-    makeEmpty() {
-        this.buildSet([]);
-    }
+  makeGlider() {
+    this.buildSet([
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [1, 0],
+      [0, 1],
+    ]);
+  }
 
-    makePentominoR() {
-        this.buildSet([[0, 0], [0, 1], [1, 0], [0, -1], [-1, -1]]);
-    }
+  makeLWSS() {
+    this.buildSet([
+      [-3, 3],
+      [0, 3],
+      [1, 2],
+      [-3, 1],
+      [1, 1],
+      [-2, 0],
+      [-1, 0],
+      [0, 0],
+      [1, 0],
+    ]);
+  }
 
-    makeGlider() {
-        this.buildSet([[-1, -1], [0, -1], [1, -1], [1, 0], [0, 1]]);
-    }
+  makeF() {
+    this.buildSet([
+      [-2, 6],
+      [-1, 6],
+      [0, 6],
+      [-2, 5],
+      [-2, 4],
+      [-1, 4],
+      [-2, 3],
+      [-2, 2],
+      [-2, 1],
+    ]);
+  }
 
-    makeLWSS() {
-        this.buildSet([[-3, 3], [0, 3], [1, 2], [-3, 1], [1, 1], [-2, 0], [-1, 0], [0, 0], [1, 0]]);
-    }
+  makeHWSS() {
+    this.buildSet([
+      [-5, -1],
+      [-4, -1],
+      [-7, -2],
+      [-2, -2],
+      [-1, -3],
+      [-7, -4],
+      [-1, -4],
+      [-6, -5],
+      [-5, -5],
+      [-4, -5],
+      [-3, -5],
+      [-2, -5],
+      [-1, -5],
+    ]);
+  }
 
-    makeF() {
-        this.buildSet([[-2, 6], [-1, 6], [0, 6], [-2, 5], [-2, 4], [-1, 4], [-2, 3], [-2, 2], [-2, 1]]);
-    }
-
-    makeHWSS() {
-        this.buildSet([[-5, -1], [-4, -1], [-7, -2], [-2, -2], [-1, -3], [-7, -4], [-1, -4], [-6, -5], [-5, -5], [-4, -5], [-3, -5], [-2, -5], [-1, -5]]);
-    }
-
-    makeTagalong() {
-        this.buildSet([[-6, 7], [-5, 7], [-4, 7], [-3, 7], [-6, 6], [-2, 6], [8, 6], [-6, 5], [6, 5], [7, 5], [-5, 4], [-2, 4],
-        [1, 4], [2, 4], [8, 4], [9, 4], [10, 4], [0, 3], [1, 3], [2, 3], [9, 3], [10, 3], [11, 3], [-5, 2], [-2, 2],
-        [1, 2], [2, 2], [8, 2], [9, 2], [10, 2], [-6, 1], [6, 1], [7, 1], [-6, 0], [-2, 0], [8, 0], [-6, -1], [-5, -1], [-4, -1], [-3, -1]]);
-    }
-};
+  makeTagalong() {
+    this.buildSet([
+      [-6, 7],
+      [-5, 7],
+      [-4, 7],
+      [-3, 7],
+      [-6, 6],
+      [-2, 6],
+      [8, 6],
+      [-6, 5],
+      [6, 5],
+      [7, 5],
+      [-5, 4],
+      [-2, 4],
+      [1, 4],
+      [2, 4],
+      [8, 4],
+      [9, 4],
+      [10, 4],
+      [0, 3],
+      [1, 3],
+      [2, 3],
+      [9, 3],
+      [10, 3],
+      [11, 3],
+      [-5, 2],
+      [-2, 2],
+      [1, 2],
+      [2, 2],
+      [8, 2],
+      [9, 2],
+      [10, 2],
+      [-6, 1],
+      [6, 1],
+      [7, 1],
+      [-6, 0],
+      [-2, 0],
+      [8, 0],
+      [-6, -1],
+      [-5, -1],
+      [-4, -1],
+      [-3, -1],
+    ]);
+  }
+}
