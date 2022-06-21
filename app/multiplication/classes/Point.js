26e81ee49df31b7c7a18e9a1beb7356204380e65
Fernand SOUALO <fernand.soualo@ge.com>
2022-06-21 10:52:00 +0200

    dev: refactor Point to es6

--- app/multiplication/classes/Point.js
+++ app/multiplication/classes/Point.js
@@ -1,42 +1,38 @@
-(function() {
-	'use strict';
-	
-	var svgns = 'http://www.w3.org/2000/svg';
+var svgns = 'http://www.w3.org/2000/svg';
 
-  var cx0 = 50;
-	var cy0 = 50;
-	var r0 = 45;
+var cx0 = 50;
+var cy0 = 50;
+var r0 = 45;
 
-  function Point(x, y) {
+class Point extends Drawable {
+	constructor(x, y) {
+		super();
 		this.x = x;
 		this.y = y;
 	}
 
-	// share the function with prototype to avoid duplication in each object
-	Point.prototype.draw = function () {
+	static getFromAngle = function (angle) {
+		var cx = cx0 + r0 * Math.cos(angle);
+		var cy = cy0 + r0 * Math.sin(angle);
+
+		return new Point(cx, cy);
+	};
+
+	static getFromIndex(index, sampleNbr) {
+		return Point.getFromAngle(Point.getAngleFromIndex(index, sampleNbr));
+	}
+
+	draw() {
+		super.draw();
 		var gPoints = document.querySelector('g.points');
 		var circle = document.createElementNS(svgns, 'circle');
 		circle.setAttributeNS(null, 'cx', this.x);
 		circle.setAttributeNS(null, 'cy', this.y);
 		circle.setAttributeNS(null, 'r', 1);
 		gPoints.appendChild(circle);
-	};
+	}
 
-  // Static method of Point
-  Point.getFromAngle = function(angle) {
-    var cx = cx0 + r0 * Math.cos(angle);
-		var cy = cy0 + r0 * Math.sin(angle);
-
-    return new Point(cx, cy);
-  }
-
-  Point.getFromIndex = function(index, sampleNbr) {
-    return Point.getFromAngle(getAngleFromIndex(index, sampleNbr));
-  }
-
-  function getAngleFromIndex(index, sampleNbr) {
-    return (index * Math.PI * 2) / sampleNbr;
-  }
-
-  window.Point = Point;
-})();
+	static getAngleFromIndex(index, sampleNbr) {
+		return (index * Math.PI * 2) / sampleNbr;
+	}
+}
